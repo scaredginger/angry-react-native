ab40033b98ebc54de5a706a2d24f4c301189a0c3
Andrew Kallasmae <akal0002@student.monash.edu>
2017-08-05 13:18:50 +1000

    Made a simple python server.

---
+++ server/server.py
@@ -0,0 +1,30 @@
+import time
+from http import server
+
+#HOST_NAME = 'example.net' # !!!REMEMBER TO CHANGE THIS!!!
+PORT_NUMBER = 8000
+
+class MobileHandler(server.BaseHTTPRequestHandler):
+    def do_HEAD(s):
+        s.send_response(200)
+        s.send_header(bytes("Content-type", "utf-8"), bytes("text/html", "utf-8"))
+        s.end_headers()
+    def do_GET(s):
+        s.send_response(200)
+        s.send_header(bytes("Content-type", "utf-8"), bytes("text/html", "utf-8"))
+        s.end_headers()
+        s.wfile.write(bytes("<html><head><title>Title goes here.</title></head>", "utf-8"))
+        s.wfile.write(bytes("<body><p>This is a test.</p>", "utf-8"))
+        s.wfile.write(bytes("<p>You accessed path: </p>", "utf-8"))
+        s.wfile.write(bytes("</body></html>", "utf-8"))
+
+server_class = server.HTTPServer
+httpd = server_class(("", PORT_NUMBER), MobileHandler)
+print (time.asctime(), "Server Starts - " + str(PORT_NUMBER))
+
+try:
+    httpd.serve_forever()
+except KeyboardInterrupt:
+    pass
+httpd.server_close()
+print (time.asctime(), "Server Stops - " + str(PORT_NUMBER))
