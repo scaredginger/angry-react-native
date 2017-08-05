82af0a025501bc5dede63273597a6a280e44afe4
Dylan Kay <dylan.kay@me.com>
2017-08-05 17:12:48 +1000

    serves menu
    also fixed json

--- server/server.py
+++ server/server.py
@@ -1,9 +1,35 @@
 import time
+import os.path
 from http import server
 import mysql.connector
+import json
 
 #HOST_NAME = 'example.net' # !!!REMEMBER TO CHANGE THIS!!!
 PORT_NUMBER = 8000
+
+def respond_unknown (s, path):
+    s.send_response(400)
+    s.send_header(bytes("Content-type", "utf-8"), bytes("text/html", "utf-8"))
+    s.end_headers()
+    s.wfile.write(bytes("Invalid request", "utf-8"))
+
+def respond_menu (s, restaurant_id):
+    if os.path.isfile(restaurant_id + ".json"):
+        s.send_response(200)
+        s.send_header(bytes("Content-type", "utf-8"), bytes("application/json", "utf-8"))
+        s.end_headers()
+
+        menu_file = open(restaurant_id + ".json")
+        s.wfile.write(bytes(menu_file.read(), "utf-8"))
+    else:
+        s.send_response(404)
+        s.send_header(bytes("Content-type", "utf-8"), bytes("text/html", "utf-8"))
+        s.end_headers()
+
+        s.wfile.write(bytes("Restaurant not found", "utf-8"))
+
+
+
 
 class MobileHandler(server.BaseHTTPRequestHandler):
     def do_HEAD(s):
@@ -11,13 +37,19 @@ class MobileHandler(server.BaseHTTPRequestHandler):
         s.send_header(bytes("Content-type", "utf-8"), bytes("text/html", "utf-8"))
         s.end_headers()
     def do_GET(s):
-        s.send_response(200)
-        s.send_header(bytes("Content-type", "utf-8"), bytes("text/html", "utf-8"))
-        s.end_headers()
-        s.wfile.write(bytes("<html><head><title>Title goes here.</title></head>", "utf-8"))
-        s.wfile.write(bytes("<body><p>This is a test.</p>", "utf-8"))
-        s.wfile.write(bytes("<p>You accessed path: </p>", "utf-8"))
-        s.wfile.write(bytes("</body></html>", "utf-8"))
+        path = s.path.split("/")
+        path.pop(0)
+
+        if path[0] == "menu":
+            restaurant_id = path[1]
+
+            respond_menu(s, restaurant_id)
+        elif path[0] == "order":
+            restaurant_id = path[1]
+
+        else:
+            respond_unknown(s, s.path)
+
 
 server_class = server.HTTPServer
 httpd = server_class(("", PORT_NUMBER), MobileHandler)
