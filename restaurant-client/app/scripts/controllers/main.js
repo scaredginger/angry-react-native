b6c97f8afbd66a9a8d26bef39cb8cb796d65534f
Dylan Kay <dylan.kay@me.com>
2017-08-06 01:28:08 +1000

    Angular scaffolding

---
+++ restaurant-client/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name restaurantClientApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the restaurantClientApp
+ */
+angular.module('restaurantClientApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
